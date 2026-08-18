7319cf7fd5a93e64de026168d5c3053b9cbe0826
zainebbouaziz-ux <zaineb.bouaziz@fsb.ucar.tn>
2026-08-18 10:41:28 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-﻿from .metadata import RadiometricParams, read_dji_metadata
-from .converter import ThermalImage, load_thermal_image
-
-__all__ = ["RadiometricParams", "read_dji_metadata", "ThermalImage", "load_thermal_image"]
